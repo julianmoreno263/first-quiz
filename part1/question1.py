d9b4ee5ad789b107027438ac982503a6d8494ee4
Julián Moreno <julianmoreno263@gmail.com>
2023-10-31 19:51:40 -0500

    part1 solution problems 1 and 2

--- part1/question1.py
+++ part1/question1.py
@@ -1,10 +1,10 @@
 ################################################################################
 #     ____                          __     _                          ___
 #    / __ \  __  __  ___    _____  / /_   (_)  ____    ____          <  /
-#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \         / / 
-#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        / /  
-#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /_/   
-#                                                                        
+#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \         / /
+#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        / /
+#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /_/
+#
 #  Question 1
 ################################################################################
 #
@@ -14,22 +14,27 @@
 # Read the test suite to know the values that these functions should return.
 
 def get_city_temperature(city):
-   if city == "Quito":
-      return 22
-   if city == "Sao Paulo":
-      return 17
-   if city == "San Francisco":
-      return 16
+    if city == "Quito":
+        return 22
+    if city == "Sao Paulo":
+        return 17
+    if city == "San Francisco":
+        return 16
+    if city == "New York":
+        return 14
+
 
 def get_city_weather(city):
 
-  sky_condition = None
+    sky_condition = None
 
-  if city == "Sao Paulo":
-     sky_condition = "cloudy"
-  elif city == "New York":
-     sky_condition = "rainy"
+    if city == "Sao Paulo":
+        sky_condition = "cloudy"
+    elif city == "New York":
+        sky_condition = "rainy"
+    elif city == "Quito":
+        sky_condition = "sunny"
 
-  temperature = get_city_temperature(city)
+    temperature = get_city_temperature(city)
 
-  return str(temperature) + " degrees and " + sky_condition
+    return str(temperature) + " degrees and " + sky_condition
--- part1/question2.py
+++ part1/question2.py
@@ -1,21 +1,29 @@
 ################################################################################
-#     ____                          __     _                          ___ 
+#     ____                          __     _                          ___
 #    / __ \  __  __  ___    _____  / /_   (_)  ____    ____          |__ \
 #   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \         __/ /
-#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        / __/ 
-#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /____/ 
-#                                                                         
+#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        / __/
+#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /____/
+#
 #  Question 2
 ################################################################################
 #
 # Instructions:
-# Write a function that will swap a tuple of two items. For example, the function 
-# should change (x, y) into (y, x). 
+# Write a function that will swap a tuple of two items. For example, the function
+# should change (x, y) into (y, x).
 # Assign the function to `swapper` so that the function `run_swapper(..)` can use
-# it. As always, there is a test suite that checks the result. It is in 
+# it. As always, there is a test suite that checks the result. It is in
 # `question2_test.py.`
 
-swapper = None
+def exchange_tuple(tuple1):
+    my_list = list(tuple1)
+    my_list[0], my_list[1] = my_list[1], my_list[0]
+    my_tuple = tuple(my_list)
+    return my_tuple
+
+
+swapper = exchange_tuple
+
 
 def run_swapper(list_of_tuples):
-  return list(map(swapper, list_of_tuples))
+    return list(map(swapper, list_of_tuples))
